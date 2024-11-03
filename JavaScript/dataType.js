1f2e5d3874211a1fd218a20a6e17cd2ffd243fad
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-03 17:31:19 +0000

    dataTypes

---
+++ JavaScript/dataType.js
@@ -0,0 +1,20 @@
+let name = "Amit Pattanayak"  //String DataType
+//console.log(name);
+
+let age = 22                         //Number DataType
+let logInUser_Id = true             // Boolean DataType
+
+
+/*String => ""
+Boolean => true/false
+null => standalone value
+undefined =>
+Symbol => unique*/
+
+
+// Object
+
+console.log(typeof "Amit");
+
+console.table([typeof name,age,logInUser_Id]);
+
