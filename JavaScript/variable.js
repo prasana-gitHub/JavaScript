315b5ecd91666702bef3a304120a5b290582bb8c
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-03 17:31:02 +0000

    variable

---
+++ JavaScript/variable.js
@@ -0,0 +1,6 @@
+const account_Id = 2566655
+let accountName = "Amit Pattanayak"
+var city = "Khallikote"
+console.log(accountName);
+let accountCIFNumber
+console.table([account_Id,accountName,city,accountCIFNumber])
