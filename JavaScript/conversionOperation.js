718d546b531a054fcf11bca443718e733fc61de8
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-03 18:23:02 +0000

    conversionOperation

---
+++ JavaScript/conversionOperation.js
@@ -0,0 +1,44 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score)); //using method 
+
+
+let number = "25";
+console.log(typeof number); // o/p String
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);  // after conversion o/p will be number (String to number)
+
+
+let digit = "256sbckscbv";
+let findDataType = Number(digit);
+console.log(typeof findDataType);        //  number
+console.log(findDataType);              //   o/p NaN  
+
+/*33 => number
+"25" => string->number
+"256sbckscbv" => NaN      Basic Conversion
+ "true"=> 1 false => 0*/
+
+
+
+ let loggedIn = 1 ;
+ let yt = Boolean(loggedIn)
+ console.log(yt)
+
+
+ let sbci = 0;
+ let ohs = Boolean(sbci)
+ console.log(ohs);
+
+ let OKX = "call me Pattanayak"
+ let story = Boolean(OKX)
+ console.log(story)
+
+ let sjcons = ""
+ let cahv = Boolean(sjcons)
+ console.log(cahv)
+ 
+
+ // 1 = true; 0 = false;
+ //"call me pattanayak" = 1;
+ //"" = false;
