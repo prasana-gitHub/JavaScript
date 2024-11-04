aa17b68467c6aac6e2aa1d799db07197342d0541
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-04 08:29:12 +0000

    operations

---
+++ JavaScript/operations.js
@@ -0,0 +1,26 @@
+let value = 3; 
+let negValue = -value;
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-3);
+console.log(2/3);
+console.log(2*3);
+console.log(2**3);
+console.log(2%3);*/
+
+let name = "Amit";
+let name1 = " Pattanayak";
+
+let name2 = name + name1
+console.log(name2);
+
+console.log("1"+2);   //  O/P - 12
+console.log(1+2+"1"); // O/P - 31
+
+
+let gameCounter = 256
+++gameCounter;
+gameCounter--;
+
+console.log(gameCounter);
