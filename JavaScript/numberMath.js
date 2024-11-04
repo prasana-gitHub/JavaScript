59aea91d04956eb516a2580c10740f6e80f560b5
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-04 17:33:09 +0000

    numberMath

---
+++ JavaScript/numberMath.js
@@ -0,0 +1,27 @@
+const myBalance = 20000
+console.log(myBalance.toString().length);
+console.log(myBalance.toLocaleString());
+console.log(myBalance.toFixed(2));
+console.log(myBalance.toPrecision(4));
+
+// **************Maths*****************
+
+/*console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.abs(-4));
+console.log(Math.acos(256));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.5));
+console.log(Math.round(4.2));*/
+
+
+
+console.log(Math.max(4,8,9,6,1,4));
+console.log(Math.min(4,8,6,7,4,2));
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min)+1));
