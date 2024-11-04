72d8a05d0d84d1e9c2e2f338d1fb446bf7f4181b
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-04 08:45:04 +0000

    comparision

---
+++ JavaScript/comparision.js
@@ -0,0 +1,16 @@
+console.log(null>0);
+console.log(null<0);
+console.log(null == 0);
+console.log(null != 0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined == 0);
+console.log(undefined!=0);
+
+
+
+
+
+// === (Strict Check)
+console.log("256" === 256);
