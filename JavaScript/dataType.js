8bbcce67a3427ca036a7eef64756692e69f0a093
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-04 09:08:26 +0000

    dataType

--- JavaScript/dataType.js
+++ JavaScript/dataType.js
@@ -18,3 +18,21 @@ console.log(typeof age);
 
 console.table([typeof name, typeof age,typeof logInUser_Id])
 
+
+
+let str1 = "AMit";
+let str2 = String(str1)
+console.log(typeof str2);
+
+let id = 29515
+let idType = Number(id)
+console.log(typeof (idType));
+
+
+let x = 26449
+let scib = Symbol(x)
+console.log(typeof (scib));
+
+
+let njc 
+console.log(typeof (njc));
